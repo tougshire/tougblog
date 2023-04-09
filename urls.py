ce32fced0be5ee0bcd512ab86b83fbbb7757c046
tougshire <87095019+tougshire@users.noreply.github.com>
2023-04-09 18:15:12 -0400

    Change Sdchomepage to Tougblog

--- urls.py
+++ urls.py
@@ -2,13 +2,13 @@ from django.views.generic.base import RedirectView
 from django.urls import path, reverse_lazy
 from . import views
 
-app_name = 'sdchomepage'
+app_name = 'tougblog'
 
 urlpatterns = [
-    path('', RedirectView.as_view(url=reverse_lazy('sdchomepage:homepage'))),
+    path('', RedirectView.as_view(url=reverse_lazy('tougblog:homepage'))),
     path('homepage/', views.HomePage.as_view(), name='homepage'),
     path('post/<int:pk>/detail/', views.PostDetail.as_view(), name='post'),
     path('page/<slug:slug>/detail/', views.PageDetail.as_view(), name='page'),
     path('event/<int:pk>/detail/', views.EventDetail.as_view(), name='event')
 
-]
+]   
